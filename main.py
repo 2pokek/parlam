7b95296f09f47ef5792f5419dd3e59add58352ca
2pokek <alkuzmich6479@gmail.com>
2022-12-01 22:41:33 +0300

    2

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
-for i in range(0,740,20):
+persons_url_list = []
+
+for i in range(0, 740, 20):
     url = f'https://www.bundestag.de/ajax/filterlist/en/members/453158-453158/h_a45203fd0f1592191f1bda63b5d86d72?limit=20&noFilterSet=true&offset={i}'
 
     q = requests.get(url)
     result = q.content
 
-    soup = BeautifulSoup(result,'lxml')
+    soup = BeautifulSoup(result, 'lxml')
+    persons = soup.find_all(class_='bt-open-in-overlay')
+
+    for person in persons:
+        person_page_url = person.get('href')
+        persons_url_list.append(person_page_url)
+
+with open('persons_url_list.txt', 'a') as file:
+    for line in persons_url_list:
+        file.write(f'{line}\n')
