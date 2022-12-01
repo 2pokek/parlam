6f4a1074bb9685d339c2ef547c5007b2b4b3e3f8
2pokek <alkuzmich6479@gmail.com>
2022-12-01 23:14:38 +0300

    3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+import json
 from time import sleep
 import requests
 from bs4 import BeautifulSoup
@@ -24,3 +25,39 @@ from bs4 import BeautifulSoup
 # with open('persons_url_list.txt', 'a') as file:
 #     for line in persons_url_list:
 #         file.write(f'{line}\n')
+
+with open('persons_url_list.txt') as file:
+    lines = [line.strip() for line in file.readline()]
+
+    data_dict = []
+    count = 0
+
+    for line in lines:
+        q = requests.get(line)
+        result = q.content
+
+        soup = BeautifulSoup(result, 'lxml')
+        person = soup.find(class_='bt-biografie-name').find('h3').text
+        person_name_company = person.strip().split(',')
+        person_name = person_name_company[0]
+        person_company = person_name_company[1].strip()
+
+        person_sm = soup.find_all(class_='bt-link-extern')
+
+        person_sm_urls = []
+        for item in person_sm:
+            person_sm_urls.append(item.get('href'))
+
+        data = {
+            "person_name": person_name,
+            "company_name": person_company,
+            "sm": person_sm_urls
+        }
+
+        count += 1
+        print(f'#{count}: {line} is done')
+
+        data_dict.append(data)
+
+        with open('data.json', 'w') as json_file:
+            json.dump(data_dict, json_file, indent=3)
